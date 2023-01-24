76f83b3c4d3e1015b5714d44e5fb0d5b4e3ee669
Mikhal Bukhtiyarov <113313154+Atropka@users.noreply.github.com>
2023-01-24 22:23:43 +0300

    first deploy

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(93, 429);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(217, 40);
+			this.label1.TabIndex = 9;
+			this.label1.Text = "Антон Котов";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(370, 435);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(44, 31);
+			this.label2.TabIndex = 10;
+			this.label2.Text = "19";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label3.Location = new System.Drawing.Point(97, 469);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(122, 20);
+			this.label3.TabIndex = 11;
+			this.label3.Text = "Знак зодиака";
+			// 
+			// button4
+			// 
+			this.button4.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.tinder;
+			this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button4.FlatAppearance.BorderSize = 0;
+			this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button4.Location = new System.Drawing.Point(133, 14);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(51, 43);
+			this.button4.TabIndex = 8;
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button2
+			// 
+			this.button2.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.brotherhood;
+			this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button2.FlatAppearance.BorderSize = 0;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.button2.Location = new System.Drawing.Point(43, 14);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(56, 42);
+			this.button2.TabIndex = 7;
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button5
+			// 
+			this.button5.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.settings;
+			this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button5.FlatAppearance.BorderSize = 0;
+			this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button5.Location = new System.Drawing.Point(213, 14);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(54, 43);
+			this.button5.TabIndex = 6;
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.InitialImage = null;
+			this.pictureBox1.Location = new System.Drawing.Point(100, 93);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(314, 339);
+			this.pictureBox1.TabIndex = 3;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.MistyRose;
+			this.button3.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.love;
+			this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button3.FlatAppearance.BorderSize = 0;
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button3.Location = new System.Drawing.Point(314, 492);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(100, 96);
+			this.button3.TabIndex = 2;
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			this.button3.MouseCaptureChanged += new System.EventHandler(this.button3_MouseCaptureChanged);
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.MistyRose;
+			this.button1.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.close;
+			this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonShadow;
+			this.button1.FlatAppearance.BorderSize = 0;
+			this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Location = new System.Drawing.Point(100, 492);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(100, 96);
+			this.button1.TabIndex = 0;
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.MouseCaptureChanged += new System.EventHandler(this.button1_MouseCaptureChanged);
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.Snow;
+			this.panel1.Controls.Add(this.button2);
+			this.panel1.Controls.Add(this.button4);
+			this.panel1.Controls.Add(this.button5);
+			this.panel1.Location = new System.Drawing.Point(101, 7);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(314, 69);
+			this.panel1.TabIndex = 12;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.MistyRose;
+			this.ClientSize = new System.Drawing.Size(508, 646);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button3_MouseCaptureChanged(object sender, EventArgs e)
+		{
+this.button3.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.heart;
+		}
+		private void button1_MouseCaptureChanged(object sender, EventArgs e)
+		{
+			this.button1.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.close__1_ ;
+		}
+	}
+}
